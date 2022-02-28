df93d26996b5e9c44831593c5b9924e7b4a4030c
nutcracker22 <nutcracker@mailbox.org>
2022-02-28 22:04:22 +0100

    edited readme.md, improved movie_db.py

--- README.md
+++ README.md
@@ -6,4 +6,14 @@ First steps:
 - setup Codio virtual environment and updated python version
 - cloned GitHub repository into Codio
 - chose a dataset: https://www.kaggle.com/hamzansariii/latest-popular-movies-dataset
-- identified a need to cut part of the data (only movies between 2007 and 2022)
+- identified a need to cut part of the data (only movies between 2007 and 2022), because otherwise the database would 
+  contain more than 7000 data sets
+- designed the database using hand-drawn database diagram
+- wrote the first draft of code for movie_db.py (the database creator file)
+- setup a development branch in GitHub
+- committed the first draft to the development branch
+- ran the code and checked the error messages
+- implemented work-arounds for this and similar errors
+- ran again to create the first version of the database
+- inspected database to look for semantic errors, which might be caused by mistakes in the original dataset
+- wrote workarounds for those 
--- movie_db.py
+++ movie_db.py
@@ -1,6 +1,62 @@
 import sqlite3
 import csv
 
+def assign_values(r, i):
+    if isinstance(year, int) and int(year) >= 2007:
+        print(str(i + 2), row)
+        movie_name = row[1]
+        movie_overview = row[3]
+        try:
+            release_date = row[2]
+        except ValueError:
+            release_date = "not known"
+        try:
+            if len(row[4]) <= 3 and row[4] != "":
+                original_language = row[4]
+            else:
+                original_language = "not known"
+        except ValueError:
+            original_language = "not known"
+        try:
+            vote_count = int(row[5])
+        except ValueError:
+            vote_count = "not known"
+        try:
+            if float(row[6]) <= 10:
+                vote_average = float(row[6])
+            else:
+                vote_average = "not known"
+        except ValueError:
+            vote_average = "not known"
+        try:
+            adult = bool(row[7])
+        except ValueError:
+            adult = "not known"
+
+        #            cur.execute('INSERT INTO movies VALUES(NULL,?,?)', (movie_name, movie_overview))
+        try:
+            cur.execute('INSERT INTO release_dates VALUES(NULL,?)', (release_date,))
+        except sqlite3.IntegrityError:
+            pass
+        try:
+            cur.execute('INSERT INTO languages VALUES(NULL,?)', (original_language,))
+        except sqlite3.IntegrityError:
+            pass
+        try:
+            cur.execute('INSERT INTO age_ratings VALUES(NULL,?)', (adult,))
+        except sqlite3.IntegrityError:
+            pass
+        try:
+            cur.execute('INSERT INTO vote_counts VALUES(NULL,?)', (vote_count,))
+        except sqlite3.IntegrityError:
+            pass
+        try:
+            cur.execute('INSERT INTO vote_averages VALUES(NULL,?)', (vote_average,))
+        except sqlite3.IntegrityError:
+            pass
+        conn.commit()
+
+
 conn = sqlite3.connect('movies-data.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
 cur = conn.cursor()
 
@@ -19,11 +75,11 @@ conn.execute('CREATE TABLE movies (id INTEGER PRIMARY KEY AUTOINCREMENT, name TE
              'FOREIGN KEY(release_id) REFERENCES release_date(id), FOREIGN KEY(language_id) REFERENCES language(id)'
              'FOREIGN KEY(age_rating_id) REFERENCES age_rating(id), FOREIGN KEY(vote_count_id) REFERENCES vote_count(id),'
              'FOREIGN KEY(vote_average_id) REFERENCES vote_average(id) )')
-conn.execute('CREATE TABLE release_dates ( id INTEGER PRIMARY KEY AUTOINCREMENT, release_date DATETIME DEFAULT)')
-conn.execute('CREATE TABLE languages (id INTEGER PRIMARY KEY AUTOINCREMENT, language TEXT )')
-conn.execute('CREATE TABLE age_ratings ( id INTEGER PRIMARY KEY AUTOINCREMENT, age_rating TEXT )')
-conn.execute('CREATE TABLE vote_counts ( id INTEGER PRIMARY KEY AUTOINCREMENT, vote_count INTEGER )')
-conn.execute('CREATE TABLE vote_averages ( id INTEGER PRIMARY KEY AUTOINCREMENT, vote_average REAL )')
+conn.execute('CREATE TABLE release_dates ( id INTEGER PRIMARY KEY AUTOINCREMENT, release_date TEXT UNIQUE )')
+conn.execute('CREATE TABLE languages (id INTEGER PRIMARY KEY AUTOINCREMENT, language TEXT UNIQUE )')
+conn.execute('CREATE TABLE age_ratings ( id INTEGER PRIMARY KEY AUTOINCREMENT, age_rating TEXT UNIQUE )')
+conn.execute('CREATE TABLE vote_counts ( id INTEGER PRIMARY KEY AUTOINCREMENT, vote_count INTEGER UNIQUE)')
+conn.execute('CREATE TABLE vote_averages ( id INTEGER PRIMARY KEY AUTOINCREMENT, vote_average REAL UNIQUE )')
 print("Tables created successfully.")
 
 with open('latest_popular_movies_dataset.csv', newline='') as file:
@@ -31,20 +87,10 @@ with open('latest_popular_movies_dataset.csv', newline='') as file:
     next(reader)
     i = 0
     for row in reader:
-        if int(row[2].split("-", 1)[0]) >= 2007:
-            print(i, row)
-            movie_name = row[1]
-            movie_overview = row[3]
-            release_date = row[2]
-            original_language = row[4]
-            vote_count = int(row[5])
-            vote_average = float(row[6])
-            adult = bool(row[7])
-
-        cur.execute('INSERT INTO movies VALUES(NULL,?,?)', (movie_name, movie_overview))
-        cur.execute('INSERT INTO release_dates VALUES(NULL,?)', (release_date))
-        cur.execute('INSERT INTO languages VALUES(NULL,?)', (original_language))
-        cur.execute('INSERT INTO age_ratings VALUES(NULL,?)', (adult))
-        cur.execute('INSERT INTO vote_counts VALUES(NULL,?)', (vote_count))
-        cur.execute('INSERT INTO vote_averages VALUES(NULL,?)', (vote_average))
-        conn.commit()
+        year = row[2].split("-", 1)[0]
+        try:
+            year = int(year)
+            assign_values(row, i)
+            i += 1
+        except ValueError:
+            print(year)
