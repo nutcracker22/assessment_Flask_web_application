5de8bef1c5a6299c35db065f409c64e75f21cad8
nutcracker22 <nutcracker@mailbox.org>
2022-03-04 23:17:58 +0100

    refactor

---
+++ sites/test.py
@@ -0,0 +1,39 @@
+import os
+from flask import Flask, Blueprint, flash, g, redirect, render_template, request, session, url_for
+
+def create_app(test_config=None): #code mainly from Flask Documentation/Tutorial
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'movies-data.db'),
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    from . import index
+    app.register_blueprint(index.bp)
+
+    from . import database
+    database.init_app(app)
+
+    from . import movies
+    app.register_blueprint(movies.bp)
+
+    from . import movie_details
+    app.register_blueprint(movie_details.bp)
+
+    from . import year
+    app.register_blueprint(year.bp)
+
+    from . import statistics
+    app.register_blueprint(statistics.bp)
+
+    return app
